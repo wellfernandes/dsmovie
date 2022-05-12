8a638d247fc230cc991b15d891ed86e100a79759
Welliton Fernandes Leal <wellfernandes@me.com>
2022-05-11 23:48:45 -0300

    implementado navigation buttons

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,6 +1,6 @@
-import {ReactComponent as StarFull} from 'assets/img/StarFull.svg';
-import {ReactComponent as StarHalf} from 'assets/img/StarHalf.svg';
-import {ReactComponent as StarEmpty} from 'assets/img/StarEmpty.svg';
+import { ReactComponent as StarFull } from 'assets/img/StarFull.svg';
+import { ReactComponent as StarHalf } from 'assets/img/StarHalf.svg';
+import { ReactComponent as StarEmpty } from 'assets/img/StarEmpty.svg';
 import './styles.css'
 
 function MovieStars() {
